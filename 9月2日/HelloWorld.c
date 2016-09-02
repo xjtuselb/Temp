0e9c2061007eaaca0de6b60f8cc1dbf32f2e1016
hest0001 <hest0001@outlook.com>
2016-09-02 11:24:44 +0800

    9/2

---
+++ 9月2日/HelloWorld.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main() {
+	printf("This is a C program.\n");
+	printf("This is the first program.\n");
+}
---
+++ 9月2日/Helloword.cpp
@@ -0,0 +1,8 @@
+﻿#include<iostream>
+using namespace std;
+
+void main() {
+	cout << "hello world!" << "\n";
+	cout << "这是我的第一个CPP程序。" << endl;
+}
+
